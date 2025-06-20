396ab4a7b3bb7bd4231afc82cf9b32a21b1a2174
Imran Qureshi <imranq2@hotmail.com>
2025-06-20 08:41:59 -0700

    update to node 24

--- {{cookiecutter.directory_name}}/jest.config.ts
+++ {{cookiecutter.directory_name}}/jest.config.ts
@@ -1,15 +1,9 @@
-import {createDefaultPreset, JestConfigWithTsJest} from 'ts-jest'
+import type { Config } from 'jest'
+import { createDefaultPreset } from 'ts-jest'
 
-const presetConfig = createDefaultPreset({});
-
-const jestConfig: JestConfigWithTsJest = {
-    ...presetConfig,
-    testEnvironment: 'node',
-    testMatch: ['**/test/**/*.spec.ts', '**/test/**/*.ts'],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.ts',
-        '!<rootDir>/src/types/**/*.ts',
-    ],
+const config: Config = {
+  // [...]
+  ...createDefaultPreset(),
 }
 
-export default jestConfig;
+export default config
